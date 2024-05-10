ce2003bfc393afede7e014c39ae6bdf1a6e6512b
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-05-10 23:54:50 +0500

    Adding Functionalities for Deletation Button

--- index.js
+++ index.js
@@ -2,32 +2,57 @@ let userInput = document.getElementById("task-input");
 console.log(userInput);
 
 function pushToTaskList() {
-    let task = userInput.value ;
+    let task = userInput.value;
     console.log(task);
-    if(task === ""){
+    if (task === "") {
         alert("Please enter a task to add to list");
         return;
     }
-
     let todoListContainer = document.getElementById("list-container");
     // Task List Item 
     let taskListItem = document.createElement("div");
     taskListItem.className = "listItem";
-    // console.log(taskListItem);
 
     // Task List Para 
     let taskOutputPara = document.createElement("p");
     taskOutputPara.innerText = task;
-    taskOutputPara.setAttribute("id" , "outputPara");
+    taskOutputPara.setAttribute("id", "outputPara");
     taskListItem.appendChild(taskOutputPara);
-    console.log(todoListContainer);
 
     // Task List Button
     let taskListItemButtonContainer = document.createElement("div");
     taskListItemButtonContainer.className = "listItembutton";
     taskListItem.appendChild(taskListItemButtonContainer);
-    // console.log(taskListItemChild);
 
     todoListContainer.appendChild(taskListItem);
-    // console.log(todoListContainer);
+
+    // Edit Button 
+    let editButton = document.createElement("button");
+    editButton.innerHTML = `<i class="fa-solid fa-pen"></i>`;
+    editButton.setAttribute("id", "edit-button");
+    editButton.setAttribute("onclick", "editTask()");
+    taskListItemButtonContainer.appendChild(editButton);
+
+    // Delete Button 
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+    deleteButton.setAttribute("id", "delete-button");
+    deleteButton.setAttribute("onclick", "deleteTask()");
+    taskListItemButtonContainer.appendChild(deleteButton);
+    userInput.value = " ";
 }
+
+
+
+function deleteTask() {
+    let deleteTaskButton = document.getElementById("delete-button");
+    var parentElement = deleteTaskButton.parentElement.parentElement;
+    console.log(parentElement.nodeType);
+    parentElement.setAttribute("id", "deleteButtonParent");
+    console.log(parentElement);
+    var deleteButtonParent = document.getElementById("deleteButtonParent");
+    // deleteButtonParent.style.display = "none";
+    // return;
+}
+
+
