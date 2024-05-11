c2cb224f2a0d373cb4580583fae1634bb33281d2
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-05-11 16:44:17 +0500

    Application Completed

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let userInput = document.getElementById("task-input");
 console.log(userInput);
+let task;
 
 function pushToTaskList() {
-    let task = userInput.value;
+    task = userInput.value;
     console.log(task);
     if (task === "") {
         alert("Please enter a task to add to list");
@@ -13,24 +14,35 @@ function pushToTaskList() {
     let taskListItem = document.createElement("div");
     taskListItem.className = "listItem";
 
+    // Task Output Container 
+    let taskOutputContainer = document.createElement("div");
+    taskOutputContainer.setAttribute("id", "task-output-container");
+    taskListItem.appendChild(taskOutputContainer);
+
     // Task List Para 
     let taskOutputPara = document.createElement("p");
     taskOutputPara.innerText = task;
     taskOutputPara.setAttribute("id", "outputPara");
-    taskListItem.appendChild(taskOutputPara);
+    taskOutputContainer.appendChild(taskOutputPara);
+
+    // Edit Input 
+    var editInputField = document.createElement("input");
+    editInputField.setAttribute("id", "editTaskInput");
+    editInputField.value = task;
+    taskOutputContainer.appendChild(editInputField);
+    console.log(editInputField.parentElement);
 
     // Task List Button
     let taskListItemButtonContainer = document.createElement("div");
     taskListItemButtonContainer.className = "listItembutton";
     taskListItem.appendChild(taskListItemButtonContainer);
-
     todoListContainer.appendChild(taskListItem);
 
     // Edit Button 
     let editButton = document.createElement("button");
     editButton.innerHTML = `<i class="fa-solid fa-pen"></i>`;
     editButton.setAttribute("id", "edit-button");
-    editButton.setAttribute("onclick", "editTask()");
+    editButton.setAttribute("onclick", "editTask(this)");
     taskListItemButtonContainer.appendChild(editButton);
 
     // Delete Button 
@@ -51,3 +63,28 @@ function deleteTask(element) {
 }
 
 
+function editTask(element) {
+    let paraEditParentElement = element.parentElement.parentElement.firstChild;
+    console.log(paraEditParentElement);
+    let editInput = paraEditParentElement.children[1];
+    editInput.setAttribute("onblur", "changeModifier(this)");
+    editInput.style.display = "flex";
+    let originalParaTask = paraEditParentElement.children[0];
+    originalParaTask.style.display = "none";
+
+}
+
+function changeModifier(input) {
+    let modifiedTaskValue = input.value;
+    let paragraphElement = input.parentElement.children[0];
+    paragraphElement.style.display = "flex";
+    input.style.display = "none";
+    console.log(paragraphElement);
+    console.log(modifiedTaskValue);
+    if (modifiedTaskValue === "") {
+        paragraphElement.innerText = task;
+    } else {
+        task = modifiedTaskValue;
+        paragraphElement.innerText = task;
+    }
+}
